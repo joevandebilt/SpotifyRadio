8c3ede4687b6292a6ccc29e8228d98eca14d1a68
Joe van de Bilt <joevandebilt@live.co.uk>
2022-02-16 17:20:42 +0000

    Bootstrap and custom CSS

--- Views/Global/Scripts.php
+++ Views/Global/Scripts.php
@@ -2,12 +2,12 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 <!-- Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="<?php echo GetWebsiteURL(); ?>/Scripts/bootstrap.bundle.js"></script>
 
 <script src="<?php echo GetWebsiteURL(); ?>/Scripts/api-controls.js"></script>
 <script src="<?php echo GetWebsiteURL(); ?>/Scripts/layout-controls.js"></script>
-<script src="<?php echo GetWebsiteURL(); ?>/Scripts/<?php echo CurrentFileNameWithoutExt(); ?>-controls.js"></script>
-<!-- <?php echo CurrentFileNameWithoutExt() ?> -->
+<script src="<?php echo GetWebsiteURL(); ?>/Scripts/admin-controls.js"></script>
+<script src="<?php echo GetWebsiteURL(); ?>/Scripts/room-controls.js"></script>
 
 <script type="text/javascript">
     window.addEventListener("load", function(e) {
