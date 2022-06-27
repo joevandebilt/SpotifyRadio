dee0c26744bc0ccd2b966f6cbab2c50bd102cdf4
Joe van de Bilt <joevandebilt@live.co.uk>
2022-06-27 22:43:27 +0100

    Removed unecessary permissions from scope

--- DI/DI.Spotify.php
+++ DI/DI.Spotify.php
@@ -1,7 +1,7 @@
 <?php
     class DISpotify
     {
-        const scope = "user-read-private user-read-email user-modify-playback-state user-read-currently-playing";
+        const scope = "user-modify-playback-state user-read-currently-playing";
         const response_type = "code";
         const redirect_uri = "https://spotify.nkode.uk/authorize.php";  
 
